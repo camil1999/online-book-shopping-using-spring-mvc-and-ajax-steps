a22b1306c845f283f64f772aa614ef880608b635
camil1999 <mamedovcamil@gmail.com>
2022-12-10 00:54:27 +0400

    spring project created

---
+++ projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/BookshoppingApplication.java
@@ -0,0 +1,13 @@
+package az.developia.bookshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshoppingApplication.class, args);
+	}
+
+}
