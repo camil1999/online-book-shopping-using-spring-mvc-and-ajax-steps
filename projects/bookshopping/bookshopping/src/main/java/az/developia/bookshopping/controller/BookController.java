65af081014e6a7c4e32cfd85f0a8b50c164e34a1
camil1999 <mamedovcamil@gmail.com>
2022-12-12 05:51:20 +0400

    Kitabin silinmesi 1-ci hisse

--- projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/controller/BookController.java
+++ projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -32,6 +33,7 @@ public class BookController {
 		model.addAttribute("book", book);
 		return "new-book";
 	}
+	
 
 	@PostMapping(path = "/new-book-process")
 	public String saveBook(@ModelAttribute(name = "book") Book book, Model model) {
@@ -42,6 +44,11 @@ public class BookController {
 		model.addAttribute("books", books);
 
 		return "redirect:/books";
+	}
+	@GetMapping(path = "/books/delete/{id}")
+	public String deleteBook(@PathVariable(name="id")Integer id,Model model) {
+		
+		return "books";
 	}
 
 }
