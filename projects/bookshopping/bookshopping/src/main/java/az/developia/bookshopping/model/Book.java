aa90010feaa78b89eb9887a0a7b3ccd0638ec298
camil1999 <mamedovcamil@gmail.com>
2022-12-12 21:34:14 +0400

    Hesabdan cixis addimi

--- projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/model/Book.java
+++ projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/model/Book.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+
 import lombok.Getter;
 
 import lombok.Setter;
