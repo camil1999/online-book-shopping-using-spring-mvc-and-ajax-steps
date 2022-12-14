e859cc4130af1f7988cb0370de70013215bdf757
camil1999 <mamedovcamil@gmail.com>
2022-12-14 04:57:40 +0400

    Alıcı səhifəsinin dizayn olunmasi əsas hissə

--- projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/controller/CustomerController.java
+++ projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/controller/CustomerController.java
@@ -1,15 +1,21 @@
 package az.developia.bookshopping.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CustomerController {
 
 	@GetMapping(path = "/customer")
-	public String showoCustomerPage() {
-
+	public String showoCustomerPage(Model model) {
+		ArrayList<String> books = new ArrayList<>();
+		for (int i = 1; i < 100; i++) {
+			books.add("");
+		}
+		model.addAttribute("books", books);
 		return "customer";
 	}
 
