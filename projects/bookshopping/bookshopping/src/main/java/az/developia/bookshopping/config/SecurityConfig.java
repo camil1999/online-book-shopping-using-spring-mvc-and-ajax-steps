fbe08a8ae130fc3883839082d6ecf57e103386aa
camil1999 <mamedovcamil@gmail.com>
2022-12-17 08:17:28 +0400

    İstifadəcilərin bazada qeydiyyatı

--- projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/config/SecurityConfig.java
+++ projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package az.developia.bookshopping.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,4 +19,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.authenticated().and().formLogin().loginPage("/show-login").loginProcessingUrl("/authenticate-user")
 				.permitAll().and().logout().permitAll();
 	}
+	@Autowired
+	DataSource dataSource;
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	auth.jdbcAuthentication().dataSource(dataSource);
+	 }
+
 }
