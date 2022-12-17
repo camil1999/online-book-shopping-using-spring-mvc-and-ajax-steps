25a41b2b7c756e5e95f64685310b90cd986e626a
camil1999 <mamedovcamil@gmail.com>
2022-12-17 06:16:52 +0400

    Alıcı səhifəsində kitab məlumatlarının göstərilməsi

--- projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/controller/CustomerController.java
+++ projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/controller/CustomerController.java
@@ -1,20 +1,24 @@
 package az.developia.bookshopping.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import az.developia.bookshopping.dao.BookDAO;
+import az.developia.bookshopping.model.Book;
+
 @Controller
 public class CustomerController {
+	@Autowired
+	private BookDAO bookDAO;
 
 	@GetMapping(path = "/customer")
 	public String showoCustomerPage(Model model) {
-		ArrayList<String> books = new ArrayList<>();
-		for (int i = 1; i < 100; i++) {
-			books.add("");
-		}
+		List<Book> books = bookDAO.findAll();
 		model.addAttribute("books", books);
 		return "customer";
 	}
