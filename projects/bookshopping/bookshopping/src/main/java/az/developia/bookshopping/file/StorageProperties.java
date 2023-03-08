9fde861724725cdcb18463a6a96928c2c1264cc4
camil1999 <mamedovcamil@gmail.com>
2023-03-08 16:19:07 +0400

    Fayl metodlarinin teyini.

--- projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/file/StorageProperties.java
+++ projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/file/StorageProperties.java
@@ -2,7 +2,10 @@ package az.developia.bookshopping.file;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Data;
+@Data
 @ConfigurationProperties("storage")
 public class StorageProperties {
+	private String location = "upload-dir-book-shopping-spring-ajax";
 
 }
--- projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/file/StorageService.java
+++ projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/file/StorageService.java
@@ -1,5 +1,19 @@
 package az.developia.bookshopping.file;
 
+import java.nio.file.Path;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface StorageService {
+	void init();
+
+	String store(MultipartFile file);
+
+	Path load(String filename);
+
+	Resource loadAsResource(String filename);
+
+	void deleteAll();
 
 }
