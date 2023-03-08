30dca2a86d4a4daf2c09b6dbfca5aed2788b3034
camil1999 <mamedovcamil@gmail.com>
2023-03-08 16:50:25 +0400

    Faylin yadda saxlanmasi metodunun kodlari.

--- projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/file/FileSystemStorageService.java
+++ projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/file/FileSystemStorageService.java
@@ -1,13 +1,17 @@
 package az.developia.bookshopping.file;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -23,19 +27,30 @@ public class FileSystemStorageService implements StorageService {
 	@Override
 	public void init() {
 		try {
-			 Files.createDirectories(rootLocation);
-			 }
-			 catch (IOException e) {
-			 throw new StorageException("Qovluq yaradila bilmedi", e);
-			 }
-			 
+			Files.createDirectories(rootLocation);
+		} catch (IOException e) {
+			throw new StorageException("Qovluq yaradila bilmedi", e);
+		}
 
 	}
 
 	@Override
 	public String store(MultipartFile file) {
+		String filename = StringUtils.cleanPath(file.getOriginalFilename());
+		String randomFileName = "";
+		try {
+			try (InputStream inputStream = file.getInputStream()) {
+				String originalFileName = file.getOriginalFilename();
+				UUID uuid = UUID.randomUUID();
+				randomFileName = originalFileName
+						.replace(originalFileName.substring(0, originalFileName.lastIndexOf(".")), uuid.toString());
+				Files.copy(inputStream, this.rootLocation.resolve(randomFileName), StandardCopyOption.REPLACE_EXISTING);
+			}
+		} catch (IOException e) {
+			throw new StorageException("Fayl yadda saxlana bilmedi: " + filename, e);
+		}
+		return randomFileName;
 
-		return null;
 	}
 
 	@Override
