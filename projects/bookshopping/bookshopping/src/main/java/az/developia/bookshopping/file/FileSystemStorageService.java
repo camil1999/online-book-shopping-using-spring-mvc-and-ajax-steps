f71e92ed4ecd499b0949d69aa636b147dabb7fdd
camil1999 <mamedovcamil@gmail.com>
2023-03-08 16:31:37 +0400

    Fayl sinifinin metodlarinin yaradilmasi.

--- projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/file/FileSystemStorageService.java
+++ projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/file/FileSystemStorageService.java
@@ -1,5 +1,41 @@
 package az.developia.bookshopping.file;
 
-public class FileSystemStorageService {
+import java.nio.file.Path;
+
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileSystemStorageService implements StorageService {
+
+	@Override
+	public void init() {
+		
+	}
+
+	@Override
+	public String store(MultipartFile file) {
+		
+		return null;
+	}
+
+	@Override
+	public Path load(String filename) {
+		
+		return null;
+	}
+
+	@Override
+	public Resource loadAsResource(String filename) {
+		
+		return null;
+	}
+
+	@Override
+	public void deleteAll() {
+		
+		
+	}
  
 }
