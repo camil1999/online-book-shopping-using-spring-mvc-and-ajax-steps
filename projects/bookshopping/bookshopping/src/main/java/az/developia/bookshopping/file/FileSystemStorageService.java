7e8e366231333dea2d15c10c2ac9e8e992eac901
camil1999 <mamedovcamil@gmail.com>
2023-03-09 02:38:44 +0400

    Fayl endirmek kodlari

--- projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/file/FileSystemStorageService.java
+++ projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/file/FileSystemStorageService.java
@@ -2,6 +2,7 @@ package az.developia.bookshopping.file;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,6 +11,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -56,13 +58,25 @@ public class FileSystemStorageService implements StorageService {
 	@Override
 	public Path load(String filename) {
 
-		return null;
+		return rootLocation.resolve(filename);
 	}
 
 	@Override
 	public Resource loadAsResource(String filename) {
 
-		return null;
+		try {
+            Path file = load(filename);
+            Resource resource = new UrlResource(file.toUri());
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new StorageFileNotFoundException(
+                        "Fayl oxuna bilmədi: " + filename);
+
+            }
+        } catch (MalformedURLException e) {
+            throw new StorageFileNotFoundException("Fayl oxuna bilmədi: " + filename, e);
+        }
 	}
 
 	@Override
