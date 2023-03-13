905c55946239625e1997e3ac48a3f5fe770e5c96
camil1999 <mamedovcamil@gmail.com>
2023-03-13 19:18:06 +0400

    Lazim olmayan linklarin gizladilmesi addimi.

--- projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/controller/HomeController.java
+++ projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/controller/HomeController.java
@@ -1,14 +1,26 @@
 package az.developia.bookshopping.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import az.developia.bookshopping.config.MySession;
 
 @Controller
 public class HomeController {
 
-	@GetMapping(path = { "/home", "/" })
-	public String showHomePage() {
+	@Autowired
+	private MySession mySession;
 
+	@GetMapping(path = { "/home", "/" })
+	public String showHomePage(Model model) {
+		String username = mySession.getUsername();
+		if (username.equals("anonymousUser")) {
+
+		} else {
+			model.addAttribute("username", username);
+		}
 		return "home";
 	}
 }
